5fe861a30807dae2ee83d765115e9afc50e9bf27
ronanskie <ronansmedeman@gmail.com>
2018-12-06 16:50:56 +0100

    first html code and css added

---
+++ ludo/public/javascripts/main.js
@@ -0,0 +1,7 @@
+function Paper(color, number){
+   this.color = color;
+   this.number = number;
+   playerIcon = document.getElementById(yellow_paper);
+}
+
+var yellowPaper1 = new Paper("yellow", "1");
